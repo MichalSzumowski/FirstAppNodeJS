374bf94c9672d0aebd4cf524d3e6eb72b358c383
Michał Szumowski <mic.szumowski@gmail.com>
2021-10-04 20:12:22 +0200

    Fix issues with Code Review

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const os = require('os');
 const fs = require('fs');
 
-// console.log('Platform: ', os.platform());
-// console.log('Arch: ', os.arch());
-// console.log('User:', os.userInfo());
-
 const genders = ['Male', 'Female'];
 const maleNames = ['Adam', 'Michał', 'Robert', 'Paweł', 'Piotr'];
 const femaleNames = ['Ewa', 'Ania', 'Asia', 'Ola', 'Karolina'];
@@ -15,11 +11,14 @@ function randChoice(arr){
   return Math.floor(Math.random() * arr.length);
 }
 
-
+function getRandomInt(min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 for (let i = 0; i < 20; i++){
   let randomName = '';
-  let newPerson = {};
   const newGender = genders[randChoice(genders)];
 
   if (newGender === 'Male') {
@@ -28,30 +27,23 @@ for (let i = 0; i < 20; i++){
     randomName = femaleNames[randChoice(femaleNames)];
   }
 
-  let randomSurname = lastNames[randChoice(lastNames)];
+  const randomSurname = lastNames[randChoice(lastNames)];
 
-  function getRandomInt(min, max){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  newObj = {
+  const newObj = {
     gender: newGender,
     firstName: randomName,
     lastName: randomSurname,
     age: getRandomInt(18, 78),
     phone: getRandomInt(700000000,999999999),
-    email: randomName + '.' + randomSurname + '@gmail.com'
+    email: `${randomName}.${randomSurname}@gmail.com`
   }
+
   people.push(newObj);
 }
 
-console.log(people);
-  
-let jsonData = JSON.stringify(people);
+const jsonData = JSON.stringify(people);
 
 fs.writeFile('people.json', jsonData, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
+  if (err) throw err;
+  console.log('The file has been saved!');
 });
